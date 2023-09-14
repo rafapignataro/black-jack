f374a585568e63fc82149993c42de7103b72cd7c
Rafael Pignataro <rafapignataro@gmail.com>
2023-09-14 14:17:54 -0300

    fix: dont show dealer count until is the end

--- src/components/Dealer.tsx
+++ src/components/Dealer.tsx
@@ -17,6 +17,8 @@ type DealerProps = {
 export function Dealer({ dealer }: DealerProps) {
   const position =  { top: 0, left: '50%', x: '-50%', y: '-50%' };
 
+  const hidingCard = dealer.cards.find(card => !card.show);
+
   return (
     <div 
       className={`player absolute flex justify-center items-center rounded-full w-32 h-32 border-4 bg-indigo-600 border-indigo-800`}
@@ -30,7 +32,7 @@ export function Dealer({ dealer }: DealerProps) {
         DEALER
       </span>
       <div className="absolute -bottom-1/4 left-1/3 flex items-end gap-4">
-        {!!dealer.count && <span className="text-sm font-bold text-white">{dealer.count}</span>}
+        {(!!dealer.count && hidingCard) && <span className="text-sm font-bold text-white">{dealer.count}</span>}
         <div className="flex items-center">
           {dealer.cards.map((card, index) => <Card key={`${card.label}_${card.suit}`} card={card} index={index} />)}
         </div>
