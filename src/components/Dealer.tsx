e6884a7cef4a5f54da0d207931c4b156519b591f
Rafael Pignataro <rafapignataro@gmail.com>
2023-09-14 22:54:00 -0300

    fix: fixed dealer count

--- src/components/Dealer.tsx
+++ src/components/Dealer.tsx
@@ -17,7 +17,7 @@ type DealerProps = {
 export function Dealer({ dealer }: DealerProps) {
   const position =  { top: 0, left: '50%', x: '-50%', y: '-50%' };
 
-  const hidingCard = dealer.cards.find(card => !card.show);
+  const hidingCard = !!dealer.cards.find(card => !card.show);
 
   return (
     <div 
@@ -32,7 +32,7 @@ export function Dealer({ dealer }: DealerProps) {
         DEALER
       </span>
       <div className="absolute -bottom-1/4 left-1/3 flex items-end gap-4">
-        {(!!dealer.count && hidingCard) && <span className="text-sm font-bold text-white">{dealer.count}</span>}
+        {(!!dealer.count && !hidingCard) && <span className="text-sm font-bold text-white">{dealer.count}</span>}
         <div className="flex items-center">
           {dealer.cards.map((card, index) => <Card key={`${card.label}_${card.suit}`} card={card} index={index} />)}
         </div>
